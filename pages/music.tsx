3f51870a73b87705ce58e4413c47f1dc8074e2ed
Adam Hill <ahill86@gmail.com>
2021-06-21 22:02:03 -0400

    Create Nav component and placeholder routes

---
+++ pages/music.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/layout';
+
+export default function Music(): JSX.Element {
+  return (
+    <Layout page='Music'>
+      Music
+    </Layout>
+  );
+}
---
+++ pages/projects.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/layout';
+
+export default function Projects(): JSX.Element {
+  return (
+    <Layout page='Projects'>
+      Projects
+    </Layout>
+  );
+}
